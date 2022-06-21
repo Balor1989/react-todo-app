9976f1d91be380cc391844cbf93fb2f160255141
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-06-21 23:34:36 +0300

    adds App component

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <>
+      <header>My Todo List</header>
+      <main>
+        <div>
+          <input type="text" />
+          <ul>
+            <li>Learn React</li>
+            <li>Learn Vue</li>
+          </ul>
+        </div>
+      </main>
+      <footer>2022</footer>
+    </>
+  );
+};
+
+export default App;
