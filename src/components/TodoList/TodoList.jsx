f389984795481577de46c9d2340b9f7e257e1ed7
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-06-26 21:22:32 +0300

    adds bootstrap

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,14 +1,14 @@
 import TodoListItem from "../TodoListItem/TodoListItem";
 
 const TodoList = ({ todos }) => {
-  const element = todos.map((item) => {
+  const element = todos.map(({ id, task }) => {
     return (
-      <li key={item.id}>
-        <TodoListItem task={item.task} />
+      <li key={id} className="list-group-item">
+        <TodoListItem task={task} />
       </li>
     );
   });
-  return <ul>{element}</ul>;
+  return <ul className="list-group">{element}</ul>;
 };
 
 export default TodoList;
