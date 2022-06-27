477b4aba65c1d14dd6db7dcb12acbeaaad496708
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-06-27 10:41:26 +0300

    adds class StatusFilter and TodoListItem

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,19 +1,23 @@
 // import s from './StatusFilter.module.css';
 
-const StatusFilter = () => {
-  return (
-    <div className="btn-group">
-      <button type="button" className="btn btn-info">
-        All
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Active
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Done
-      </button>
-    </div>
-  );
-};
+import { Component } from 'react';
+
+class StatusFilter extends Component {
+  render() {
+    return (
+      <div className="btn-group">
+        <button type="button" className="btn btn-info">
+          All
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Active
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Done
+        </button>
+      </div>
+    );
+  }
+}
 
 export default StatusFilter;
--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,25 +1,45 @@
 import s from './TodoListItem.module.css';
 import { BsFillTrashFill, BsExclamationLg } from 'react-icons/bs';
+import { Component } from 'react';
 
-const TodoListItem = ({ task, important = false }) => {
-  const importantActive = {
-    color: important ? '#8A2BE2' : '#000',
-    fontWeight: important ? 'bold' : 'normal',
+class TodoListItem extends Component {
+  state = { done: false };
+
+  taskHendler = () => {
+    console.log(`click! ${this.props.task}`);
   };
 
-  return (
-    <div className={`d-flex justify-content-between ${s.todoListItem}`}>
-      <p style={importantActive}>{task}</p>
-      <div>
-        <button type="button" className="btn btn-outline-success btn-sm float-right">
-          <BsExclamationLg />
-        </button>
+  render() {
+    const { done } = this.state;
+    const { task, important = false } = this.props;
 
-        <button type="button" className="btn btn-outline-danger btn-sm float-right">
-          <BsFillTrashFill />
-        </button>
+    let classNames = `${s.task} `;
+    if (done) {
+      classNames += 'text-decoration-line-through';
+    }
+
+    const importantActive = {
+      color: important ? '#8A2BE2' : '#000',
+      fontWeight: important ? 'bold' : 'normal',
+    };
+
+    return (
+      <div className={`d-flex justify-content-between ${s.todoListItem}`}>
+        <p className={classNames} style={importantActive} onClick={this.taskHendler}>
+          {task}
+        </p>
+        <div>
+          <button type="button" className="btn btn-outline-success btn-sm float-right">
+            <BsExclamationLg />
+          </button>
+
+          <button type="button" className="btn btn-outline-danger btn-sm float-right">
+            <BsFillTrashFill />
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 export default TodoListItem;
